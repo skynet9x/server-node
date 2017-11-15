0f9569893ee76616702bf8e9a8b0b06f019e1d7d
skynet9x <binhauto9x@gmail.com>
2017-11-16 00:48:27 +0700

    update nodejs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# server-node
+# test
+
+a [Sails](http://sailsjs.org) application
---
+++ app.js
@@ -0,0 +1,14 @@
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+app.set('port', (process.env.PORT || 4000));
+
+app.listen(app.get('port'), function ()
+{
+	console.log('Node app is running on port', app.get('port'));
+});
